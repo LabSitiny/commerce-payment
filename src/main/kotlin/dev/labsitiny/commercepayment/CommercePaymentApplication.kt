e85c358cadc67dce98842e46512a998b52788270
Parkseongheum <ab6449@gmail.com>
2022-11-27 19:29:44 +0900

    Init: Project payment service

---
+++ src/main/kotlin/dev/labsitiny/commercepayment/CommercePaymentApplication.kt
@@ -0,0 +1,11 @@
+package dev.labsitiny.commercepayment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CommercePaymentApplication
+
+fun main(args: Array<String>) {
+	runApplication<CommercePaymentApplication>(*args)
+}
---
+++ src/test/kotlin/dev/labsitiny/commercepayment/CommercePaymentApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.labsitiny.commercepayment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommercePaymentApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
